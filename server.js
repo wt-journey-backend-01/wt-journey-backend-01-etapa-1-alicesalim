d16ec96d25bb50f1dadd79dc401199a6ffec5b19
alicesalim <alice.salim.khouri@gmail.com>
2025-07-15 12:25:23 -0300

    corrigindo

--- server.js
+++ server.js
@@ -1,23 +1,91 @@
 const express = require('express')
-const path = require ('path');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
+
 app.get('/contato', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'contato.html'));
+    res.sendFile(path.join(__dirname, 'views', 'contato.html'));
 });
+
+app.post('/contato-recebido', (req, res) => {
+    const { nome, email, assunto, mensagem } = req.body;
+
+    res.status(200).send(`
+        <!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Contato</title>
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+
+<body>
+    <header>
+        <h1>Contato Recebido</h1>
+    </header>
+
+    <section>
+        <a href="/">Voltar</a>
+        <p>Ola ${nome} recebemos sua mensagem sobre ${assunto}, vamos encaminhar para nossa equipe e te daremos um retorno atraves do email ${email}</p>
+    </section>
+</body>
+
+</html>
+        
+        `)
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor da DevBurger rodando em localhost:${PORT}`);
 });
 
 app.get('/api/lanches', (req, res) => {
-  const lanches = require('./public/data/lanches.json');
-  res.json(lanches);
+    const lanches = require('./public/data/lanches.json');
+    res.json(lanches);
+});
+
+app.get('/sugestao', (req, res) => {
+    const nome = req.query.nome;
+    const ingredientes = req.query.ingredientes;
+    res.status(200).send(`
+        <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DevBurguer</title>
+    <link rel="stylesheet" href="/css/style.css" />
+</head>
+
+<body>
+<header>
+<h1>Obrigada pela seugestão</h1>
+</header>
+<section>
+<a href="/"> <- Voltar </a>
+<p>Recebemos a seugestão do lanche: ${nome} <br> Que contem os ingredientes: ${ingredientes}<p>
+
+</section>
+</body>
+
+</html>
+        `);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'))
 });
